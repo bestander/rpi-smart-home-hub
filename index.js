40682a9f303464689b978afa1738d62c22ead9e9
Konstantin Raev <bestander@gmail.com>
2015-04-15 01:05:40 +1200

    first commit, sending and receiving data from bluno

---
+++ index.js
@@ -0,0 +1,37 @@
+var noble = require('noble');
+
+noble.on('stateChange', function(state) {
+    if (state === 'poweredOn') {
+        noble.startScanning();
+    } else {
+        noble.stopScanning();
+    }
+});
+
+var BlunoServiceUUID = 'dfb0';
+var BlunoServiceSerialCharacteristicUUID = 'dfb1';
+
+noble.on('discover', function(peripheral) {
+    if(peripheral.advertisement.localName && peripheral.advertisement.localName.indexOf('Bluno') !== -1) {
+        peripheral.connect(function(error) {
+            if(error) {
+                console.log("error connecting", error)
+            }
+        });
+        peripheral.on('connect', function () {
+            console.log("connected to ", peripheral.advertisement.localName);
+
+            peripheral.discoverServices([BlunoServiceUUID], function(err, services) {
+                var serialCharacteristic;
+                services[0].discoverCharacteristics([BlunoServiceSerialCharacteristicUUID], function(error, characteristics) {
+                    serialCharacteristic = characteristics[0];
+                    console.log("sending to serialCharacteristic " + serialCharacteristic.uuid);
+                    serialCharacteristic.write(new Buffer("test", "utf-8"), true);
+                    serialCharacteristic.on('data', function(data, isNotification){
+                        console.log("Data from " + serialCharacteristic.uuid + ": " + data + ": length: " + data.length, data);
+                    });
+                })
+            });
+        });
+    }
+});
