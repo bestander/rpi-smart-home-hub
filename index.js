562cb7f862fb412ba7763a4516d965188615974a
Konstantin Raev <bestander@gmail.com>
2015-04-29 00:11:24 +1200

    debugged code, supported agreed protocol

--- index.js
+++ index.js
@@ -1,36 +1,54 @@
 var noble = require('noble');
 
-noble.on('stateChange', function(state) {
-    if (state === 'poweredOn') {
-        noble.startScanning();
-    } else {
-        noble.stopScanning();
-    }
-});
-
 var BlunoServiceUUID = 'dfb0';
 var BlunoServiceSerialCharacteristicUUID = 'dfb1';
 
 noble.on('discover', function(peripheral) {
     if(peripheral.advertisement.localName && peripheral.advertisement.localName.indexOf('Bluno') !== -1) {
+        console.log("discovered Bluno");
+        noble.stopScanning();
         peripheral.connect(function(error) {
             if(error) {
-                console.log("error connecting", error)
+                console.error("error connecting", error);
+                return;
             }
-        });
-        peripheral.on('connect', function () {
             console.log("connected to ", peripheral.advertisement.localName);
+            peripheral.discoverSomeServicesAndCharacteristics([BlunoServiceUUID], [BlunoServiceSerialCharacteristicUUID], function(err, services, characteristics) {
+                var serialCharacteristic = characteristics[0];
+                //                console.log("sending to serialCharacteristic " + serialCharacteristic.uuid);
+                //                serialCharacteristic.write(new Buffer([1]), true);
+                console.log("waiting for data from Bluno");
+                serialCharacteristic.on('data', function(data, isNotification) {
+                    // TODO how about parsing incomplete message
+                    /**
+                     * Message format:
+                     * 5 bytes
+                     * 1 - beginning of the message 0xFF
+                     * 2 - isRaining: 1 raining / 0 not raining
+                     * 3 - buzzerMode: 1 singing / 0 not singing
+                     * 4,5 - sensorReading [low, high]
+                     */
+                    if(data.length === 5 && data[0] === 0xFF) {
+                        var isRaining = data[1];
+                        var buzzerMode = data[2];
+                        var reading = data[3] + (data[4] << 8);
+                        console.log("got reading:", isRaining, buzzerMode, reading);
+                        peripheral.disconnect(function(){
+                            console.log("disconnected from Bluno");
+                            setTimeout(function(){
+                                noble.startScanning()
+                            }, 2000);
+                        });
 
-            peripheral.discoverServices([BlunoServiceUUID], function(err, services) {
-                services[0].discoverCharacteristics([BlunoServiceSerialCharacteristicUUID], function(error, characteristics) {
-                    var serialCharacteristic = characteristics[0];
-                    console.log("sending to serialCharacteristic " + serialCharacteristic.uuid);
-                    serialCharacteristic.write(new Buffer([1]), true);
-                    serialCharacteristic.on('data', function(data, isNotification){
-                        console.log("Data from " + serialCharacteristic.uuid + ": " + data + ": length: " + data.length, data);
-                    });
-                })
+                    } else {
+                        console.error("Could not parse data from sensor", data)
+                    }
+                });
+
             });
+
         });
     }
 });
+
+noble.startScanning();
